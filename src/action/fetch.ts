68a3bb8c28dc2b110e6830e203e8689bcf4f4f3b
windowdong11 <windowdong11@naver.com>
2021-01-04 20:32:58 +0900

    Endpoints에 MiddleServer추가
    MiddleServerData interface 추가
    중간 서버에 테스트 요청하기 위한 데이터 목록

--- src/action/fetch.ts
+++ src/action/fetch.ts
@@ -1,12 +1,22 @@
+import { Url } from "url"
+
 export const Endpoints  = {
     GraphQL : `http://localhost:${process.env.REACT_APP_GQL_PORT}/graphql`,
     ProtoBuf : `http://localhost:${process.env.REACT_APP_PROTO_PORT}/`,
     RestAPI : `http://localhost:${process.env.REACT_APP_REST_PORT}/`,
+    MiddleServer : `http://localhost:${process.env.REACT_APP_MIDDLE_SERVER_PORT}/`,
 }
 
 export enum HttpMethod {
     Post = "POST",
     Get = "GET",
+}
+
+export interface MiddleServerData {
+    address: string,
+    duration: number,
+    arrivalRate: number,
+    clientCount: number,
 }
 
 async function fetchAPI(endpoint: string, query : string, method: HttpMethod) {
