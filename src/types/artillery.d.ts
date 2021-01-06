b55761442c3a84fea6996c3f33a99169284ea79a
windowdong11 <windowdong11@naver.com>
2021-01-07 00:39:39 +0900

    middle(artillery) 서버 response body 인터페이스 정의

---
+++ src/types/artillery.d.ts
@@ -0,0 +1,86 @@
+interface Artillery {
+    "aggregate": {
+        "timestamp": "2021-01-06T15:11:53.401Z",
+        "scenariosCreated": 1,
+        "scenariosCompleted": 1,
+        "requestsCompleted": 1,
+        "latency": {
+            "min": 10.7,
+            "max": 10.7,
+            "median": 10.7,
+            "p95": 10.7,
+            "p99": 10.7
+        },
+        "rps": {
+            "count": 1,
+            "mean": 2.13
+        },
+        "scenarioDuration": {
+            "min": 66.7,
+            "max": 66.7,
+            "median": 66.7,
+            "p95": 66.7,
+            "p99": 66.7
+        },
+        "scenarioCounts": {
+            "0": 1
+        },
+        "errors": {},
+        "codes": {
+            "200": 1
+        },
+        "matches": 0,
+        "customStats": {},
+        "counters": {},
+        "scenariosAvoided": 0,
+        "phases": [
+            {
+                "duration": "1",
+                "arrivalRate": "1",
+                "mode": "uniform"
+            }
+        ]
+    },
+    "intermediate": [
+        {
+            "timestamp": "2021-01-06T15:11:53.398Z",
+            "scenariosCreated": 1,
+            "scenariosCompleted": 1,
+            "requestsCompleted": 1,
+            "latency": {
+                "min": 10.7,
+                "max": 10.7,
+                "median": 10.7,
+                "p95": 10.7,
+                "p99": 10.7
+            },
+            "rps": {
+                "count": 1,
+                "mean": 2.13
+            },
+            "scenarioDuration": {
+                "min": 66.7,
+                "max": 66.7,
+                "median": 66.7,
+                "p95": 66.7,
+                "p99": 66.7
+            },
+            "scenarioCounts": {
+                "0": 1
+            },
+            "errors": {},
+            "codes": {
+                "200": 1
+            },
+            "matches": 0,
+            "latencies": [
+                10671459
+            ],
+            "customStats": {},
+            "counters": {},
+            "concurrency": 0,
+            "pendingRequests": 0,
+            "scenariosAvoided": 0
+        }
+    ]
+}
