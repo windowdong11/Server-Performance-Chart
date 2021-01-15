9d90431daf270352231453ed89e73c5987b556a3
windowdong11 <windowdong11@naver.com>
2021-01-15 23:16:43 +0900

    Fix : gap 설정시 가끔 데이터 나타나지 않는 버그 수정
    원인 : Math.floor을 올림으로 착각함.
    해결방법 : Math.floor을 Math.ceil로 변경하여 해결

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -17,47 +17,58 @@ const options = {
 
 interface parseChartDataResult {
     labels: number[],
+    max: number,
+    min: number,
+    median: number,
     data: GraphData,
 }
 
 function parseChartData(json: Artillery, gap : number) : parseChartDataResult {
     let latencies = json.intermediate[0].latencies.sort((a, b) => a - b)
     const min = latencies[0], max = latencies[latencies.length - 1]
-    const lines = Math.floor((max - min) / gap) + 1//10
+    const lines = Math.ceil((max - min) / gap) + 1//10
     const labels = Array.from(Array(lines), (_, index) => min + gap * index)
-    const labelAreas = Array(0, ...labels.map((value) => Math.floor(value + gap / 2)))
+    const labelAreas = Array(...labels.map((value) => Math.floor(value + gap / 2)))
     let data = Array(lines).fill(0)
     latencies.forEach((value) => {
-        for (let index = 1; index < labelAreas.length; index++) {
+        for (let index = 0; index < labelAreas.length; index++) {
             if (value <= labelAreas[index]) {
-                data[index - 1]++
+                data[index]++
                 return
             }
         }
     })
+    console.log(latencies)
+    console.log(data)
     return {
         labels: labels,
+        max,
+        min,
+        median: json.intermediate[0].latency.median,
         data: {
             label: "test", // Should change to input or dev-defined value
             backgroundColor: "", // Should define
             borderColor: "", // Should define
             fill: true,
             data: data
-        }
+        },
     }
 }
 
 export default function Chart(chartProps : ChartProps) {
     //const [isProcessed, setIsProcessed] = useState<boolean[]>([])
     const graphData : GraphData[] = []
-    let label : string[] = []
+    let label : number[] = []
+    let prevMax : number = 0
     chartProps.jsons.forEach((json, index) => {
-        const {labels, data} = parseChartData(json, chartProps.gap)
+        const {labels, max, data} = parseChartData(json, chartProps.gap)
         graphData.push(data)
-        label = labels.toString().split(',')
+        if(prevMax < max){
+            label = labels
+        }
     })
     const chartData : ChartData = {
-        labels: label,
+        labels: label.toString().split(','),
         datasets: graphData,
     }
     return (
